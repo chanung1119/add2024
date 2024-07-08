e86638a75a19c0bf45036d493f0f0f02bd877184
chanung <71597214+chanung1119@users.noreply.github.com>
2024-07-09 01:00:35 +0900

    Add files via upload
    분자 모형의 3d 파일을 만든다.
    TODO: flutter project와 python file의 결합 필요

---
+++ makeGlb.py
@@ -0,0 +1,57 @@
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import trimesh
+from pygltflib import GLTF2
+
+def smiles_to_glb(smiles, output_file):
+    mol = Chem.MolFromSmiles(smiles)
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(mol)
+    AllChem.UFFOptimizeMolecule(mol)
+
+    conf = mol.GetConformer()
+    atoms = []
+    bonds = []
+    for atom in mol.GetAtoms():
+        pos = conf.GetAtomPosition(atom.GetIdx())
+        atoms.append({
+            "element": atom.GetSymbol(),
+            "x": pos.x,
+            "y": pos.y,
+            "z": pos.z
+        })
+        
+    for bond in mol.GetBonds():
+        start = bond.GetBeginAtomIdx()
+        end = bond.GetEndAtomIdx()
+        type = bond.GetBondType()
+        bonds.append({"start": start, "end": end, "type": type})
+        print(f"start: {start}, end: {end}, type: {type}")
+        
+
+    spheres = []
+    for atom in atoms:
+        sphere = trimesh.creation.uv_sphere(radius=0.4)
+        sphere.visual.vertex_colors = [255, 0, 0] if atom["element"] == "C" else [0, 255, 0]
+        sphere.apply_translation([atom["x"], atom["y"], atom["z"]])
+        spheres.append(sphere)
+     
+    cylinders = []   
+    for bond in bonds:
+        startX = atoms[bond["start"]]["x"]
+        startY = atoms[bond["start"]]["y"]
+        startZ = atoms[bond["start"]]["z"]
+        endX = atoms[bond["end"]]["x"]
+        endY = atoms[bond["end"]]["y"]
+        endZ = atoms[bond["end"]]["z"]
+        cylinder = trimesh.creation.cylinder(radius=0.15 ,segment=[[startX, startY, startZ], [endX, endY, endZ]])
+        cylinder.visual.vertex_colors = [255, 255, 255]
+        cylinders.append(cylinder)
+        
+    objects = spheres + cylinders
+    scene = trimesh.Scene(objects)
+    scene.export(output_file, file_type='glb')
+
+smiles = 'CN=C=O'
+output_file = 'molecule.glb'
+smiles_to_glb(smiles, output_file)
